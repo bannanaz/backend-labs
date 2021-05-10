b8588445a2ee83c843dc393ec76dcc2fefc1c616
bannanaz <anlepet@gmail.com>
2021-05-10 10:00:37 +0200

    add user(post), update user(put)& delete user(delete)

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,7 @@ const router = (app) => {
       response.send(result);
     });
   });
+
   // Display a single user by ID
   app.get("/users/:id", (request, response) => {
     const id = request.params.id;
@@ -22,6 +23,40 @@ const router = (app) => {
       if (error) throw error;
 
       response.send(result);
+    });
+  });
+
+  // Add a new user
+  app.post("/users", (request, response) => {
+    pool.query("INSERT INTO users SET ?", request.body, (error, result) => {
+      if (error) throw error;
+
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+  });
+
+  // Update an existing user
+  app.put("/users/:id", (request, response) => {
+    const id = request.params.id;
+
+    pool.query(
+      "UPDATE users SET ? WHERE id = ?",
+      [request.body, id],
+      (error, result) => {
+        if (error) throw error;
+
+        response.send("User updated successfully.");
+      }
+    );
+  });
+  // Delete a user
+  app.delete("/users/:id", (request, response) => {
+    const id = request.params.id;
+
+    pool.query("DELETE FROM users WHERE id = ?", id, (error, result) => {
+      if (error) throw error;
+
+      response.send("User deleted.");
     });
   });
 };
